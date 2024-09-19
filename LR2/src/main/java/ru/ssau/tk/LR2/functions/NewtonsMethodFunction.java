08667f8e43c128c65b0feff019b8b9c6337f2397
KOT BACbKA <vagenet5122@gmail.com>
2024-09-19 21:41:53 +0400

    Task 1 X and Y: Newton's Method

--- LR2/src/main/java/ru/ssau/tk/LR2/functions/NewtonsMethodFunction.java
+++ LR2/src/main/java/ru/ssau/tk/LR2/functions/NewtonsMethodFunction.java
@@ -1,2 +1,38 @@
-package ru.ssau.tk.LR2.functions;public class NewtonsMethodFunction {
+package ru.ssau.tk.LR2.functions;
+
+public class NewtonsMethodFunction implements MathFunction {
+
+    private final static double DERIVATIVE_EPS = 1e-6;
+    private final MathFunction func;
+    private final double tolerance;
+
+    public NewtonsMethodFunction(MathFunction func, double tolerance) {
+        this.func = func;
+        this.tolerance = tolerance;
+    }
+
+    private double derivative(double x) {
+        // double precision hack
+        return func.apply(x + DERIVATIVE_EPS) / DERIVATIVE_EPS / 2 - func.apply(x - DERIVATIVE_EPS) / DERIVATIVE_EPS / 2;
+    }
+
+    @Override
+    public double apply(double x) {
+        for (int i = 0; i < 100000000; i++) {
+            double y = func.apply(x);
+            double y_prime = derivative(x);
+
+            if (Math.abs(y_prime) < tolerance) break;
+
+            double new_x = x - y / y_prime;
+
+            if (Math.abs(new_x - x) < tolerance) {
+                return new_x;
+            }
+
+            x = new_x;
+        }
+
+        return Double.NaN; // function does not converge to zero
+    }
 }
--- LR2/src/test/java/ru/ssau/tk/LR2/functions/NewtonsMethodFunctionTest.java
+++ LR2/src/test/java/ru/ssau/tk/LR2/functions/NewtonsMethodFunctionTest.java
@@ -1,4 +1,35 @@
+package ru.ssau.tk.LR2.functions;
+
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 public class NewtonsMethodFunctionTest extends TestCase {
-  
+
+    public NewtonsMethodFunctionTest( String testName )
+    {
+        super( testName );
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( NewtonsMethodFunctionTest.class );
+    }
+
+    public void testApply()
+    {
+        final double PRECISION = 1e-9;
+
+        MathFunction sin = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return Math.sin(x);
+            }
+        };
+
+        NewtonsMethodFunction sin_newton = new NewtonsMethodFunction(sin, PRECISION);
+
+        assert(Math.abs(Math.PI - sin_newton.apply(2.0)) < PRECISION);
+
+    }
 }
