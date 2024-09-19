5672c18a2060792fe3d6a1dec17f3ebc2a226407
KOT BACbKA <vagenet5122@gmail.com>
2024-09-19 21:40:31 +0400

    TabulatedFunction fix

--- LR2/src/main/java/ru/ssau/tk/LR2/functions/TabulatedFunction.java
+++ LR2/src/main/java/ru/ssau/tk/LR2/functions/TabulatedFunction.java
@@ -1,14 +1,21 @@
 package ru.ssau.tk.LR2.functions;
 
-public interface TabulatedFunction extends MathFunction{
+public interface TabulatedFunction extends MathFunction {
 
     int getCount();
+
     double getX(int index);
+
     double getY(int index);
+
     void setY(int index, double value);
+
     int indexOfX(double x);
+
     int indexOfY(double y);
+
     double leftBound();
+
     double rightBound();
 
-
+}
