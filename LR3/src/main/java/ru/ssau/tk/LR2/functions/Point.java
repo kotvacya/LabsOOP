8ab5bc2740d58bff8821af99245090a78dec6e4d
Nikulin Vladislav / Никулин Владислав <po_6706@mail.ru>
2024-10-07 21:20:17 +0400

    Task 1 Y: class Point

---
+++ LR3/src/main/java/ru/ssau/tk/LR2/functions/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.LR2.functions;
+
+public class Point {
+    final public double x;
+    final public double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
