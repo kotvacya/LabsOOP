159e5443ecef7d1b40822a3601433bffcebcd122
KOT BACbKA <vagenet5122@gmail.com>
2024-10-08 23:36:47 +0400

    LR 3 Task 2: createStrictUnmodifiable

--- LR3/src/main/java/ru/ssau/tk/LR2/functions/factory/TabulatedFunctionFactory.java
+++ LR3/src/main/java/ru/ssau/tk/LR2/functions/factory/TabulatedFunctionFactory.java
@@ -2,6 +2,7 @@ package ru.ssau.tk.LR2.functions.factory;
 
 import ru.ssau.tk.LR2.functions.StrictTabulatedFunction;
 import ru.ssau.tk.LR2.functions.TabulatedFunction;
+import ru.ssau.tk.LR2.functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
@@ -9,4 +10,10 @@ public interface TabulatedFunctionFactory {
     default TabulatedFunction createStrict(double[] xValues, double[] yValues){
         return new StrictTabulatedFunction(create(xValues, yValues));
     }
+
+    default TabulatedFunction createStrictUnmodifiable(double[] xValues, double[] yValues){
+        return new UnmodifiableTabulatedFunction(new StrictTabulatedFunction(create(xValues, yValues)));
+    }
+
+
 }
--- LR3/src/test/java/ru/ssau/tk/LR2/functions/factory/TabulatedFunctionFactoryTest.java
+++ LR3/src/test/java/ru/ssau/tk/LR2/functions/factory/TabulatedFunctionFactoryTest.java
@@ -36,6 +36,22 @@ public class TabulatedFunctionFactoryTest extends TestCase {
         assertEquals(1.0, func2.apply(1.0));
     }
 
+    public void testStrictUnmodifiable(){
+        ArrayTabulatedFunctionFactory array_factory = new ArrayTabulatedFunctionFactory();
+        TabulatedFunction func1 = array_factory.createStrictUnmodifiable(new double[]{1.0,2.0,3.0}, new double[]{1.0,2.0,3.0});
+
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> func1.apply(1.5));
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> func1.setY(1, 10.0));
+        assertEquals(1.0, func1.apply(1.0));
+
+        LinkedListTabulatedFunctionFactory linked_factory = new LinkedListTabulatedFunctionFactory();
+        TabulatedFunction func2 = linked_factory.createStrictUnmodifiable(new double[]{1.0,2.0,3.0}, new double[]{1.0,2.0,3.0});
+
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> func2.apply(1.5));
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> func2.setY(1, 10.0));
+        assertEquals(1.0, func2.apply(1.0));
+    }
+
     public TabulatedFunctionFactoryTest(String testName) {
         super(testName);
     }
