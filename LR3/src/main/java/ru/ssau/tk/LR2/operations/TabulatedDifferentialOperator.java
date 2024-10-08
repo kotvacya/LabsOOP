a99fce25b5d5386d871fca27b72dbae4c0a98ab9
Nikulin Vladislav / Никулин Владислав <po_6706@mail.ru>
2024-10-09 00:05:05 +0400

    Task 2 Y: DifferentialOperator

--- LR3/src/main/java/ru/ssau/tk/LR2/operations/TabulatedDifferentialOperator.java
+++ LR3/src/main/java/ru/ssau/tk/LR2/operations/TabulatedDifferentialOperator.java
@@ -8,36 +8,36 @@ import ru.ssau.tk.LR2.functions.factory.TabulatedFunctionFactory;
 public class TabulatedDifferentialOperator implements DifferentialOperator<TabulatedFunction> {
     private TabulatedFunctionFactory factory;
 
-    TabulatedDifferentialOperator(TabulatedFunctionFactory factory){
+    TabulatedDifferentialOperator(TabulatedFunctionFactory factory) {
         this.factory = factory;
     }
 
-    TabulatedDifferentialOperator(){
+    TabulatedDifferentialOperator() {
         factory = new ArrayTabulatedFunctionFactory();
     }
 
-    public TabulatedFunctionFactory getFactory(){
+    public TabulatedFunctionFactory getFactory() {
         return factory;
     }
 
-    public void setFactory(TabulatedFunctionFactory factory){
+    public void setFactory(TabulatedFunctionFactory factory) {
         this.factory = factory;
     }
 
     @Override
-    public TabulatedFunction derive(TabulatedFunction function){
+    public TabulatedFunction derive(TabulatedFunction function) {
         Point[] points = TabulatedFunctionOperationService.asPoints(function);
 
         double[] xValues = new double[points.length];
         double[] yValues = new double[points.length];
 
-        for(int i = 0; i < points.length - 1; i++){
+        for (int i = 0; i < points.length - 1; i++) {
             xValues[i] = points[i].x;
-            yValues[i] = (points[i+1].y-points[i].y)/(points[i+1].x-points[i].x);
+            yValues[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
         }
 
-        xValues[xValues.length-1] = points[xValues.length-1].x;
-        yValues[xValues.length-1] = yValues[xValues.length-2];
+        xValues[xValues.length - 1] = points[xValues.length - 1].x;
+        yValues[xValues.length - 1] = yValues[xValues.length - 2];
 
         return factory.create(xValues, yValues);
     }
