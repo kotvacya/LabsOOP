9a53eb5083fa9892e65416c78390f4756fefdb47
Nikulin Vladislav / Никулин Владислав <po_6706@mail.ru>
2024-10-09 20:18:47 +0400

    Task 3 Y: writeTabulatedFunction

--- LR3/src/main/java/ru/ssau/tk/LR2/io/FunctionsIO.java
+++ LR3/src/main/java/ru/ssau/tk/LR2/io/FunctionsIO.java
@@ -1,7 +1,24 @@
 package ru.ssau.tk.LR2.io;
 
+import ru.ssau.tk.LR2.functions.Point;
+import ru.ssau.tk.LR2.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
+
     public FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter pw = new PrintWriter(writer);
+        pw.println(function.getCount());
+        for (Point el : function) {
+            pw.printf("%f %f\n", el.x, el.y);
+        }
+        pw.flush();
     }
 }
