429d9fade20099325fb43c30726ad6ba971f2c5d
KOT BACbKA <vagenet5122@gmail.com>
2024-10-10 19:03:37 +0400

    fix merge conflict

--- LR3/src/main/java/ru/ssau/tk/LR2/io/FunctionsIO.java
+++ LR3/src/main/java/ru/ssau/tk/LR2/io/FunctionsIO.java
@@ -1,6 +1,5 @@
 package ru.ssau.tk.LR2.io;
 
-<<<<<<< HEAD
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
