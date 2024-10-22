7cc7b92b6ada31bcb4c1fff256cdbb4cd72d0ba5
KOT BACbKA <vagenet5122@gmail.com>
2024-10-22 22:52:11 +0400

    LR4 Task 1 fix ReadTask and WriteTask

--- LR4/src/main/java/ru/ssau/tk/LR2/concurrent/ReadTask.java
+++ LR4/src/main/java/ru/ssau/tk/LR2/concurrent/ReadTask.java
@@ -12,7 +12,9 @@ public class ReadTask implements Runnable {
     @Override
     public void run() {
         for(int i = 0; i < func.getCount(); i++){
-            System.out.printf("After read: i = %d, x = %f, y = %f%n", i, func.getX(i), func.getY(i));
+            synchronized (func) {
+                System.out.printf("After read: i = %d, x = %f, y = %f%n", i, func.getX(i), func.getY(i));
+            }
         }
     }
 }
--- LR4/src/main/java/ru/ssau/tk/LR2/concurrent/ReadWriteTaskExecutor.java
+++ LR4/src/main/java/ru/ssau/tk/LR2/concurrent/ReadWriteTaskExecutor.java
@@ -5,7 +5,7 @@ import ru.ssau.tk.LR2.functions.LinkedListTabulatedFunction;
 
 public class ReadWriteTaskExecutor {
     public static void main(String[] args) {
-        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(new ConstantFunction(-1), 0.0, 1000.0, 1001);
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(new ConstantFunction(-1.0), 0.0, 1000.0, 1001);
 
         Thread read_thread = new Thread(new ReadTask(func));
         Thread write_thread = new Thread(new WriteTask(func, 0.5));
