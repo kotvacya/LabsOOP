306306234b5068e3b3a90f325308d38f6da9fe01
KOT BACbKA <vagenet5122@gmail.com>
2024-10-23 23:45:48 +0400

    IntegralTask impovement

--- LR4/src/main/java/ru/ssau/tk/LR2/concurrent/IntegralTask.java
+++ LR4/src/main/java/ru/ssau/tk/LR2/concurrent/IntegralTask.java
@@ -2,74 +2,49 @@ package ru.ssau.tk.LR2.concurrent;
 
 import ru.ssau.tk.LR2.functions.TabulatedFunction;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
-import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.*;
 
 public class IntegralTask implements Callable<Double> {
     private final TabulatedFunction func;
-    private final double PRECISION;
-    private Double result = 0.0;
-    ForkJoinPool forkJoinPool = new ForkJoinPool();
+
 
     public IntegralTask(TabulatedFunction func) {
         this.func = func;
-        PRECISION = 0.1;
     }
 
-    public IntegralTask(TabulatedFunction func, double precision) {
-        this.func = func;
-        PRECISION = precision;
-    }
-
-    private class IntegralRecursiveAction extends RecursiveAction {
+    private class IntegralRecursiveTask extends RecursiveTask<Double> {
         private static final long serialVersionUID = -3136839265390589299L;
 
-        private final double lower_bound;
-        private final double upper_bound;
+        private final int lower_bound;
+        private final int upper_bound;
 
-        IntegralRecursiveAction(double lower_bound, double upper_bound){
+        IntegralRecursiveTask(int lower_bound, int upper_bound) {
             this.lower_bound = lower_bound;
             this.upper_bound = upper_bound;
         }
 
         @Override
-        protected void compute() {
-            if(upper_bound - lower_bound < PRECISION){
-                run();
-            }else{
-                ForkJoinTask.invokeAll(createSubtasks());
-            }
-        }
+        protected Double compute() {
+            int half = lower_bound + (upper_bound - lower_bound) / 2;
 
-        private List<IntegralRecursiveAction> createSubtasks(){
-            List<IntegralRecursiveAction> subtasks = new ArrayList<>();
-
-            double half = lower_bound+(upper_bound-lower_bound)/2;
-
-            subtasks.add(new IntegralRecursiveAction(lower_bound, half));
-            subtasks.add(new IntegralRecursiveAction(half, upper_bound));
-
-            return subtasks;
-        }
-
-        private void run(){
-            double half = lower_bound+(upper_bound-lower_bound)/2;
-
-            synchronized (func) {
-                result += func.apply(half) * (upper_bound - lower_bound);
+            if (lower_bound < upper_bound) {
+                IntegralRecursiveTask left_sum = new IntegralRecursiveTask(lower_bound, half);
+                IntegralRecursiveTask right_sum = new IntegralRecursiveTask(half + 1, upper_bound);
+                left_sum.fork();
+                right_sum.fork();
+                return left_sum.join() + right_sum.join();
+            } else {
+                double halfunc = func.getY(lower_bound) + (func.getY(lower_bound + 1) - func.getY(lower_bound)) / 2;
+                return halfunc * (func.getX(lower_bound + 1) - func.getX(lower_bound));
             }
         }
     }
 
     @Override
     public Double call() {
-        IntegralRecursiveAction task = new IntegralRecursiveAction(func.leftBound(), func.rightBound());
-        ForkJoinTask.invokeAll(task);
+        IntegralRecursiveTask task = new IntegralRecursiveTask(0, func.getCount() - 2);
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
 
-        return result;
+        return forkJoinPool.invoke(task);
     }
 }
--- LR4/src/main/java/ru/ssau/tk/LR2/concurrent/IntegralTaskExecutor.java
+++ LR4/src/main/java/ru/ssau/tk/LR2/concurrent/IntegralTaskExecutor.java
@@ -5,9 +5,9 @@ import ru.ssau.tk.LR2.functions.SqrFunction;
 
 public class IntegralTaskExecutor {
     public static void main(String[] args) {
-        ArrayTabulatedFunction func = new ArrayTabulatedFunction(new SqrFunction(), 0.0, 3.0, 1000);
+        ArrayTabulatedFunction func = new ArrayTabulatedFunction(new SqrFunction(), 0.0, 3.0, 1000000);
 
-        IntegralTask it = new IntegralTask(func, 1e-5);
+        IntegralTask it = new IntegralTask(func);
         System.out.println(it.call());
     }
 }
