cfea6cccc3db20ab778939c4a4b0f2e1cb0d335c
Nikulin Vladislav / Никулин Владислав <po_6706@mail.ru>
2024-11-11 19:25:24 +0400

    base controller

--- LR6/src/main/java/ru/ssau/tk/LR2/web/Application.java
+++ LR6/src/main/java/ru/ssau/tk/LR2/web/Application.java
@@ -2,10 +2,19 @@ package ru.ssau.tk.LR2.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/main")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("main %s!", name);
     }
 }
