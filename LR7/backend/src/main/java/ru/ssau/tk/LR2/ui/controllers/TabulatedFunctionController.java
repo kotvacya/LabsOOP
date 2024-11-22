97d690b75d775304eed5ec2f469ab9337731f519
KOT BACbKA <vagenet5122@gmail.com>
2024-11-22 22:04:13 +0400

    factory controller

--- LR7/backend/src/main/java/ru/ssau/tk/LR2/ui/controllers/TabulatedFunctionController.java
+++ LR7/backend/src/main/java/ru/ssau/tk/LR2/ui/controllers/TabulatedFunctionController.java
@@ -1,6 +1,7 @@
 package ru.ssau.tk.LR2.ui.controllers;
 
 import jakarta.security.auth.message.AuthException;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,10 +15,12 @@ import ru.ssau.tk.LR2.functions.factory.TabulatedFunctionFactory;
 import ru.ssau.tk.LR2.ui.dto.ArrayTabulatedDTO;
 import ru.ssau.tk.LR2.ui.dto.SimpleFunctionDTO;
 import ru.ssau.tk.LR2.ui.dto.SimpleTabulatedRequestDTO;
+import ru.ssau.tk.LR2.ui.dto.TabulatedFactoryDTO;
 import ru.ssau.tk.LR2.ui.exceptions.BaseUIException;
 import ru.ssau.tk.LR2.ui.exceptions.ExceptionUtils;
 import ru.ssau.tk.LR2.ui.exceptions.TabulatedCreationException;
 import ru.ssau.tk.LR2.ui.services.SimpleFunctionService;
+import ru.ssau.tk.LR2.ui.services.TabulatedFactoryService;
 import ru.ssau.tk.LR2.ui.storage.InMemoryTabulatedFunctionStorage;
 
 import java.util.List;
@@ -29,6 +32,9 @@ public class TabulatedFunctionController {
 
     @Autowired
     SimpleFunctionService functionService;
+
+    @Autowired
+    TabulatedFactoryService factoryService;
 
     @Autowired
     InMemoryTabulatedFunctionStorage storage;
@@ -46,7 +52,7 @@ public class TabulatedFunctionController {
         }
 
         try {
-            TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+            TabulatedFunctionFactory factory = storage.getCurrentFactory(SecurityContextHolder.getContext());
             TabulatedFunction tabulatedFunction = factory.create(xValues, yValues);
 
             storage.setCurrentFunction(SecurityContextHolder.getContext(), tabulatedFunction);
@@ -62,7 +68,7 @@ public class TabulatedFunctionController {
         MathFunction mathFunction = functionService.create(requestDTO.getFunction());
 
         try {
-            TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+            TabulatedFunctionFactory factory = storage.getCurrentFactory(SecurityContextHolder.getContext());
             TabulatedFunction tabulatedFunction = factory.create(mathFunction, requestDTO.getStart(), requestDTO.getEnd(), requestDTO.getCount());
 
             storage.setCurrentFunction(SecurityContextHolder.getContext(), tabulatedFunction);
@@ -87,6 +93,25 @@ public class TabulatedFunctionController {
         }
         return null;
     }
+
+    @Cacheable
+    @GetMapping("/factory/all")
+    public List<TabulatedFactoryDTO> getTabulatedFactories(){
+        return factoryService.getAllFactories();
+    }
+
+    @GetMapping("/factory")
+    public TabulatedFactoryDTO getCurrentFactory() throws AuthException{
+        return TabulatedFactoryDTO.from(storage.getCurrentFactory(SecurityContextHolder.getContext()));
+    }
+
+    @PostMapping("/factory")
+    public void setTabulatedFactory(@NonNull @RequestParam("type") String classname) throws AuthException, BaseUIException{
+        storage.setCurrentFactory(SecurityContextHolder.getContext(), factoryService.create(classname));
+    }
+
+
+
 
 }
 
